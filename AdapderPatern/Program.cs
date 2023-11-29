5c902fadb9b844535dd1703f8f861ececf16cf59
rehmanli1470ww <144922823+rehmanli1470ww@users.noreply.github.com>
2023-11-29 10:56:51 +0400

    Add project files.

---
+++ AdapderPatern/Program.cs
@@ -0,0 +1,42 @@
+﻿
+interface Enegery 
+{
+    public void ChargingTypeC();
+}
+
+class Samsung : Enegery
+{
+    public void ChargingTypeC()
+    {
+        Console.WriteLine("charging...");
+    }
+}
+
+class Redmi : Enegery
+{
+    public void ChargingTypeC()
+    {
+        Console.WriteLine("charging...");
+       
+    }
+}
+
+class Ipone
+{
+    public void ChargingUsb_c()
+    {
+        Console.WriteLine("charging...");
+
+    }
+}
+
+
+class IponeAdapder : Ipone, Enegery
+{
+    public void ChargingTypeC()
+    {
+        ChargingUsb_c();
+    }
+}
+
+
